8b09239a8f88a7cb32df2bdd092f92ed30e98648
Tolu Adesina <tadesina90@gmail.com>
2019-09-30 16:50:04 +0100

    rolling eyes emoji updated

--- src/components/RollingEyes.js
+++ src/components/RollingEyes.js
@@ -6,7 +6,7 @@ const App = () => {
     <img
       width="120px"
       height="120px"
-      src="https://d1j8pt39hxlh3d.cloudfront.net/products/previews/5KFQVEM7GGWW6GPUYFNI/2325_xzx0MKT6L1dQObWOGvC9WLaRoNTstfoN.gif"
+      src="https://d1j8pt39hxlh3d.cloudfront.net/products/previews/5KFQVEM7GGWW6GPUYFNI/2372_8ajgCUasV0xjQkuEx3QzAkxqf7EDpu9F.gif"
       alt="face with rolling eyes"
       title="face with rolling eyes"
       className="mx-auto d-block emoji__img"
