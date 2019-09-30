e907c27bfb46190c0dc2b5fc96ef3e17c6722990
Tolu Adesina <tadesina90@gmail.com>
2019-09-30 16:49:54 +0100

    Final Card created and updated for the image view

---
+++ src/components/FinalCard.js
@@ -0,0 +1,29 @@
+import React from "react";
+import captions from "./constants/captions";
+import "./card.css";
+import "./final.css";
+
+const App = ({ number, state, style }) => {
+  
+  const {caption, emoji} = captions[number];
+ 
+  return (
+    <div
+      className="card final__card"
+      style={{
+        ...style
+      }}
+    >
+      <div className="content">
+        <div className="caption__container">
+          <p className="caption">{caption}</p>
+          <div className="emoji__wrapper">{emoji}</div>
+        </div>
+        <p className="state">{state}</p>
+        <p className="site__url">www.feelingnaija.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
