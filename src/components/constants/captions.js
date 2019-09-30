b49a3853acfccfe583117dcc20be8dca76b81825
Tolu Adesina <tadesina90@gmail.com>
2019-09-30 16:50:39 +0100

    captions list updated with correct names

--- src/components/constants/captions.js
+++ src/components/constants/captions.js
@@ -6,11 +6,11 @@ import Smirk from "../Smirk";
 import RollingEyes from "../RollingEyes";
 
 const captions = [
-  { caption: "Naija 4 life !", emoji: <Love /> },
-  { caption: "Naija go dey alright !", emoji: <Wink /> },
-  { caption: "Naija just dey there !", emoji: <Smirk /> },
-  { caption: "Naija don cast !", emoji: <RollingEyes /> },
-  { caption: "Naija dey fall hand !", emoji: <Angry /> }
+  { caption: "Naija 4 life!", emoji: <Love /> },
+  { caption: "Naija go dey alright!", emoji: <Wink /> },
+  { caption: "Naija just dey there!", emoji: <Smirk /> },
+  { caption: "Naija dey fall hand!", emoji: <RollingEyes /> },
+  { caption: "Naija don cast!", emoji: <Angry /> }
 ];
 
 export default captions;
