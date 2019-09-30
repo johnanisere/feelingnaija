35dbe164610811812120e8ae2fcfd7e2286db763
Tolu Adesina <tadesina90@gmail.com>
2019-09-30 16:48:06 +0100

    Changed the markup for Angry.js to image

--- src/components/Angry.js
+++ src/components/Angry.js
@@ -3,13 +3,14 @@ import "./emoji.css";
 
 const App = () => {
   return (
-    <div className="emoji  emoji--angry">
-      <div className="emoji__face">
-        <div className="emoji__eyebrows"></div>
-        <div className="emoji__eyes"></div>
-        <div className="emoji__mouth"></div>
-      </div>
-    </div>
+    <img
+      width="120px"
+      height="120px"
+      src="https://d1j8pt39hxlh3d.cloudfront.net/products/previews/5KFQVEM7GGWW6GPUYFNI/2317_exwAWiqz2QwBOlKuVTgUEdqCbWTUO9mP.gif"
+      alt="Angry face"
+      title="Angry face"
+      className="mx-auto d-block emoji__img"
+    ></img>
   );
 };
 
