ac18c8793f716402f968c0aa165e58cf1e46ea2f
john anisere <johnanisere@gmail.com>
2019-09-30 23:07:24 +0100

    update

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = ({ children }) => (
-  <div className="container-fluid">
+  <div className="container-fluid parent">
     <div className="container">{children}</div>
   </div>
 );
